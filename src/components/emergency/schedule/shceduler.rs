f5a576d5488f57a48fd562d15b56448a02ce6e3c
NsdHSO <nechiforelsamuel@yahoo.com>
2025-05-13 12:57:21 +0300

    feat: added 1 hour

--- src/components/emergency/schedule/shceduler.rs
+++ src/components/emergency/schedule/shceduler.rs
@@ -11,7 +11,7 @@ pub async fn start_scheduler(
     db_conn: &DatabaseConnection,
 ) -> Result<(), Box<dyn std::error::Error>> {
     // Define the interval at which the task should run
-    let mut interval = interval(Duration::from_secs(3));
+    let mut interval = interval(Duration::from_secs(30*10));
 
     println!("Emergency allocation scheduler started");
 
